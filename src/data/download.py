0e23ba590216c033d15c79d491eb45a2a16915e7
Max Schrader <mcschrader@crimson.ua.edu>
2022-12-15 13:57:07 +0000

    Fixes library warnings. Switched to feather
    Signed-off-by: Max Schrader <mcschrader@crimson.ua.edu>

--- src/data/download.py
+++ src/data/download.py
@@ -1,6 +1,7 @@
 import osmnx as ox
 from shapely import wkt
 from geopandas import GeoDataFrame
+import pandas as pd
 from typing import Union, Dict, List
 from pathlib import Path
 from tqdm import tqdm
@@ -21,7 +22,7 @@ def ensure_geometry_type(
     return df
 
 
-def download_whole_city(city_name: Union[str, List[str]], save_path: Path, timeout: int = 10000):
+def download_whole_city(city_name: Union[str, List[str]], save_path: Path, timeout: int = 10000, tags: List[str] = TOP_LEVEL_OSM_TAGS):
     if type(city_name) == str:
         name = city_name
     else:
@@ -29,14 +30,22 @@ def download_whole_city(city_name: Union[str, List[str]], save_path: Path, timeo
     print(name)
     area_path = save_path.joinpath(name)
     area_path.mkdir(parents=True, exist_ok=True)
-    for tag in tqdm(TOP_LEVEL_OSM_TAGS):
-        tag_path = area_path.joinpath(tag + ".pkl")
+    for tag in tqdm(tags):
+        tag_path = area_path.joinpath(tag + ".feather")
         if not tag_path.exists():
-            tag_gdf = download_whole_osm_tag(city_name, tag, timeout)
-            if tag_gdf.empty:
+            if (tag_path.parent /  f"{tag}_is_empty.txt").exists():
                 print(f"Tag: {tag} empty for city: {city_name}")
             else:
-                tag_gdf.to_pickle(tag_path)
+                tag_gdf = download_whole_osm_tag(city_name, tag, timeout)
+                if tag_gdf.empty:
+                    # create a file holding whether the 
+                    print(f"Tag: {tag} empty for city: {city_name}")
+                    with open(tag_path.parent / f"{tag}_is_empty.txt", 'w'):
+                        pass
+                else:
+                    tag_gdf.reset_index(inplace=True)
+                    tag_gdf.drop(columns=tag_gdf.columns.intersection(['nodes', 'ways']), inplace=True)
+                    tag_gdf.to_feather(tag_path)
         else:
             print(f"Tag: {tag} exists for city: {city_name}")
 
@@ -52,7 +61,8 @@ def download_specific_tags(
     tags: Dict[str, Union[str, bool]],
     timeout: int = 10000,
 ) -> GeoDataFrame:
-    ox.config(timeout=timeout)
+    ox.settings.timeout = timeout
+    # ox.config(timeout=timeout)
     geometries_df = ox.geometries_from_place(area_name, tags=tags)
     geometries_df = ensure_geometry_type(geometries_df)
     return geometries_df
